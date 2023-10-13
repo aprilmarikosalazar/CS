2964dcedd0dc6c3dc588b7a7e18bdadd046901e4
April Mariko Salazar <99777447+aprilmarikosalazar@users.noreply.github.com>
2023-10-13 02:30:17 -0700

    Update AsstOne.py

--- AsstOne.py
+++ AsstOne.py
@@ -1,13 +1,11 @@
-""" This is my submission for Assignment One. """
+"""Welcome to Step One."""
 
 
 def main():
-    """ Prompt the user to enter their name, then greet them and state the
-    activity.
-    """
+    """Prompt user for their name; then, greet them and state activity."""
     name = input("Please enter your name: ")
     first_string = "Hi "
-    second_string = ", let's explore some historical temperatures."
+    second_string = ", let's explore the void in your brain."
     complete_string = first_string + name + second_string
     print(complete_string)
 
@@ -19,9 +17,9 @@ if __name__ == "__main__":
 """
 --- sample run #1 ---
 Please enter your name: April
-Hi April, let's explore some historical temperatures.
+Hi April, let's explore the void in your brain.
 
 --- sample run #2 ---
-Please enter your name: PyCharm
-Hi PyCharm, let's explore some historical temperatures.
+Please enter your name: Makima
+Hi Makima, let's explore the void in your brain.
 """
